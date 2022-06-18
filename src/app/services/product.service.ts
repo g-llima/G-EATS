e7630da27fad77ba694fae7cca6b81d29652fd78
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-06-18 13:51:52 -0300

    GEASTS: Resolvido bug de preço fixo dos produtos

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,6 +13,15 @@ export class ProductService {
           'https://biancazapatka.com/wp-content/uploads/2021/10/rote-bete-burger.jpg',
         preco: 5.99,
       },
+      {
+        id: 1,
+        nome: 'Hambúrguer com Crispy de Cebola',
+        tags: ['Burger'],
+        calorias: '220 - 280',
+        veg: false,
+        imgUrl: 'https://assets.unileversolutions.com/recipes-v2/230411.jpg',
+        preco: 12.4,
+      },
     ];
   }
 }
